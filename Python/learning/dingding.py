f1fafc84961c6df90cbee831508fd21b8b4b4e1a
zenggang888 <gangzeng186@gmail.com>
2020-11-25 16:30:28 +0800

    send markdown massage

--- Python/learning/dingding.py
+++ Python/learning/dingding.py
@@ -6,6 +6,7 @@ import requests
 
 url='https://oapi.dingtalk.com/robot/send?access_token=xxx'
 
+#发送普通字符串消息
 def send_msg(msg):
     """
     发送消息的函数，这里使用阿里的钉钉
@@ -19,6 +20,21 @@ def send_msg(msg):
     except Exception as e:
         return False
     return f.status_code
+
+
+#发送markdown消息
+def send_markdown_msg():
+    headers = {"Content-Type": "application/json"}
+
+    # 消息类型和数据格式参照钉钉开发文档
+    data = {
+        "msgtype": "markdown",
+        "markdown": {
+            "title": "apk下载",
+            "text": "# 这是安卓测试版本 \n ![img](https://xxx.oss-cn-hangzhou.aliyuncs.com/photos/xxx.jpg)"
+        }
+    }
+    r = requests.post(url = dingtalk_url, data=json.dumps(data), headers=headers)
 
 info="请审批工单"
 
